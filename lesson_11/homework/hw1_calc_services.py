9d613491127cebc069b3cf6d9d6b465567edc39e
Sam <sam1206morfey@gmail.com>
2022-07-08 04:34:30 +0300

    lessons 11, 12, 13  update

--- lesson_11/homework/hw1_calc_services.py
+++ lesson_11/homework/hw1_calc_services.py
@@ -1,29 +1,2 @@
 # from random import randint
-# from threading import Thread
 
-
-# async def gen_rand_numbers():
-#     numbers = [randint(0, 100) for i in range(10_000)]
-#     return numbers
-
-
-# async def summary(lst: list):
-#     await gen_rand_numbers()
-#     counter = 0
-#     for i in lst:
-#         counter += i
-#     print(f"The summ of elements in list: {counter}")
-
-
-# async def mid_average(lst):
-#     await gen_rand_numbers
-#     counter = 0
-#     for i in lst:
-#         counter += i
-#     average = counter / len(lst)
-#     print(f"the middle-average of the elements in list: {average}")
-
-
-# thread1 = Thread(target=gen_rand_numbers())
-# thread1.start()
-# thread1.join()
